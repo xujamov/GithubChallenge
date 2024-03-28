44b57e73bccae87a478ab79fd616282d1f77829e
doniyor <doniyorprince95@gmail.com>
2024-03-28 18:45:12 +0500

    Fixed calculating metrics script

--- src/main/scala/com/githubchallenge/db/repositories/sql/ContributorsSql.scala
+++ src/main/scala/com/githubchallenge/db/repositories/sql/ContributorsSql.scala
@@ -16,18 +16,16 @@ private[repositories] object ContributorsSql {
   def selectById(id: Long): doobie.ConnectionIO[Option[ContributorMetrics]] =
     fr"""
     SELECT
-      COUNT(DISTINCT pc.project_id) AS total_projects,
-      COUNT(p.id) AS total_commits,
-      COUNT(pr.id) AS total_closed_prs,
+      COUNT(DISTINCT cm.project_id) AS total_projects,
+      COUNT(cm.id) AS total_commits,
+      COUNT(CASE WHEN pr.is_closed = TRUE THEN 1 END) AS total_closed_prs,
       COUNT(CASE WHEN pr.is_closed = FALSE THEN 1 END) AS total_open_prs
     FROM
-        contributors c
+      contributors c
     LEFT JOIN
-        project_contributors pc ON c.id = pc.contributor_id
+      commits cm ON c.id = cm.contributor_id
     LEFT JOIN
-        projects p ON pc.project_id = p.id
-    LEFT JOIN
-        pull_requests pr ON c.id = pr.contributor_id
+      pull_requests pr ON c.id = pr.contributor_id
     WHERE
       c.id = $id
   """.query[ContributorMetrics].option
--- src/main/scala/com/githubchallenge/db/repositories/sql/ProjectsSql.scala
+++ src/main/scala/com/githubchallenge/db/repositories/sql/ProjectsSql.scala
@@ -16,16 +16,16 @@ private[repositories] object ProjectsSql {
   def selectById(id: Long): doobie.ConnectionIO[Option[ProjectMetrics]] =
     fr"""
     SELECT
-      COUNT(DISTINCT pc.contributor_id) AS total_contributors,
-      COUNT(p.id) AS total_commits,
-      COUNT(pr.id) AS total_closed_prs,
+      COUNT(DISTINCT cm.contributor_id) AS total_contributors,
+      COUNT(cm.id) AS total_commits,
+      COUNT(CASE WHEN pr.is_closed = TRUE THEN 1 END) AS total_closed_prs,
       COUNT(CASE WHEN pr.is_closed = FALSE THEN 1 END) AS total_open_prs
     FROM
       projects p
     LEFT JOIN
-      project_contributors pc ON p.id = pc.project_id
-    LEFT JOIN
       pull_requests pr ON p.id = pr.project_id
+    LEFT JOIN
+      commits cm ON p.id = cm.project_id
     WHERE
       p.id = $id
   """.query[ProjectMetrics].option
